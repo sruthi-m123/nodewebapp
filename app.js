6f9a0979666429ddaa54588db26263a56faa3bcc
Sruthi <sruthigangadharan277@gmail.com>
2025-05-30 19:58:09 +0530

    Added many files of user side like categories,pageNotFound

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express=require("express");
 const app=express()//server instance created
+const path =require("path");
 const env =require("dotenv").config();
-
 const db=require("./config/db");
+const userRouter=require("./routes/userRouter");
 db()
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+app.set("view engine","ejs");
+app.set("views",[path.join(__dirname,'views/user'),path.join(__dirname,'views/admin')])
+app.use(express.static(path.join(__dirname,"public")));
+
+app.use("/",userRouter);
 
 
 app.listen(process.env.PORT,()=>{
