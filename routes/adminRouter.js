9018474672aa76a885fc40ae2abdbd1bb2205e66
Sruthi <sruthigangadharan277@gmail.com>
2025-06-25 13:05:33 +0530

    categories in admin side changes

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -3,31 +3,9 @@ const router= express.Router();
 const adminController=require("../controller/admin/adminController");
 const catController=require("../controller/admin/catController")
 const customerController=require("../controller/admin/customerController")
-const upload=require('../config/multer');
+const {upload, singleUpload}=require('../config/multer');
 const {adminAuth}=require("../middlewares/auth");
 
-// const {
-// getAllCategories,
-//     getCategory,
-//    addCategory,
-//     updateCategoryStatus,
-//     updateCategory,
-//     deleteCategory,
-//     exportCategories,
-//     getCategoryDetails
-// } = require('../controller/admin/catController');
-
-// // Debug: Log the imported functions
-// console.log('Imported functions:', {
-//   getAllCategories,
-//     getCategory,
-//    addCategory,
-//     updateCategoryStatus,
-//     updateCategory,
-//     deleteCategory,
-//     exportCategories,
-//     getCategoryDetails
-// });
 
 router.get('/page_error',adminController.page_error);
 router.get('/login',adminController.loadAdminLogin);
@@ -39,11 +17,11 @@ router.get("/users",adminAuth,customerController.customerInfo);
 router.post("/toggle_block",adminAuth,customerController.toggleBlockStatus);
 // category Managment
 router.get('/categories', adminAuth,catController.getAllCategories);
-router.post('/addCategory', adminAuth, upload.single('image'), catController.addCategory);
+router.post('/addCategory', adminAuth, singleUpload, catController.addCategory);
 //id based routes
 // router.get('/categories/:id/details', adminAuth, catController.getCategoryDetails);
 router.put('/categories/:id/status', adminAuth, catController.updateCategoryStatus);
-router.post('/categories/:id/update', adminAuth, upload.single('image'), catController.updateCategory);
+router.post('/categories/:id/update', adminAuth, singleUpload, catController.updateCategory);
 router.delete('/categories/:id', adminAuth, catController.deleteCategory);
 router.get('/categories/:id', adminAuth, catController.getCategory);
 
