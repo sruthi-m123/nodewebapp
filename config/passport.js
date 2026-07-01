40ab8e8a439fd8261bc18c9644c3d8b8b732b75f
Sruthi M <sruthigangadharan277@gmail.com>
2026-07-01 10:01:46 +0530

    fix: use environment variable for Google OAuth callback URL

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,8 @@ passport.use(
     {
         clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-callbackURL: "http://localhost:3000/user/auth/google/callback"    },
+callbackURL: process.env.GOOGLE_CALLBACK_URL 
+},
     async (accessToken, refreshToken, profile, done) => {
       try {
         let user = await User.findOne({ googleId: profile.id });
